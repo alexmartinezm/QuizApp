496a44510e91d2f7417d379328d6ab2aec8f2f31
alexmartinezm <alexmdevs@gmail.com>
2018-06-11 08:43:07 +0200

    AppCenter Integration.

--- QuizHelp/QuizHelp/App.xaml.cs
+++ QuizHelp/QuizHelp/App.xaml.cs
@@ -1,4 +1,7 @@
 using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Prism;
 using Prism.Ioc;
 using Prism.Unity;
@@ -32,6 +35,9 @@ namespace QuizHelp
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=c8179b1a-f666-491b-b0a5-aecf40e46df9;" +
+                            "android=e7141224-d1cb-40b1-9de5-635362f3735a;",
+                            typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
